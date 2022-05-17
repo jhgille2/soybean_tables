b6a965e64f2467d5e98b987bdaae8e5ac465ca15
Jay <Jhgille2@ncsu.edu>
2022-05-17 09:33:44 -0400

    added stuff to the ohenotype kable script

--- R/summarise_plant_row_data.R
+++ R/summarise_plant_row_data.R
@@ -9,6 +9,21 @@
 #' @export
 summarise_plant_row_data <- function(plant_row_data) {
 
-  NULL
+  # A simple summary table of average phenotype values by cross
+  plant_row_summary <- plant_row_data %>% 
+    select(cross, yield, seed_weight, oil_dry_basis, protein_dry_basis) %>% 
+    pivot_longer(cols = c(yield, seed_weight, oil_dry_basis, protein_dry_basis), names_to = "pheno") %>% 
+    group_by(cross, pheno) %>% 
+    summarise(avg_value = mean(value, na.rm = TRUE)) %>% 
+    ungroup() %>%
+    mutate(pheno = recode(pheno, 
+                          "oil_dry_basis"     = "Oil", 
+                          "protein_dry_basis" = "Protein", 
+                          "seed_weight"       = "SDWT", 
+                          "yield"             = "Bulk Weight")) %>%
+    pivot_wider(names_from = pheno, values_from = avg_value) %>% 
+    arrange(desc(Protein)) %>% 
+    rename(Cross = cross)
+  
 
 }
--- sandbox/plant row summary table.R
+++ sandbox/plant row summary table.R
@@ -4,21 +4,22 @@ tar_load(plant_row_data)
 plant_row_summary <- plant_row_data %>% 
   select(cross, yield, seed_weight, oil_dry_basis, protein_dry_basis) %>% 
   pivot_longer(cols = c(yield, seed_weight, oil_dry_basis, protein_dry_basis), names_to = "pheno") %>% 
+  dplyr::filter(!is.na(cross)) %>%
   group_by(cross, pheno) %>% 
-  summarise(avg_value = mean(value, na.rm = TRUE), 
+  summarise(Average   = mean(value, na.rm = TRUE), 
             min_value = min(value, na.rm = TRUE), 
             max_value = max(value, na.rm = TRUE), 
             Range     = paste(min_value, max_value, sep = " - "), 
             Var       = var(value, na.rm = TRUE)) %>% 
-  select(cross, pheno, avg_value, Range, Var) %>%
+  select(cross, pheno, Average, Range, Var) %>%
   ungroup() %>%
   mutate(pheno = recode(pheno, 
                         "oil_dry_basis"     = "Oil", 
                         "protein_dry_basis" = "Protein", 
                         "seed_weight"       = "SDWT", 
                         "yield"             = "Bulk Weight")) %>%
-  pivot_wider(names_from = pheno, values_from = c(avg_value, Range, Var), names_sort = TRUE, names_vary = "slowest") %>% 
-  arrange(desc(avg_value_Protein)) %>% 
+  pivot_wider(names_from = pheno, values_from = c(Average, Range, Var), names_sort = TRUE, names_vary = "slowest") %>% 
+  arrange(desc(Average_Protein)) %>% 
   rename(Cross = cross)
 
 
@@ -38,3 +39,39 @@ get_pheno_name <- function(column_name){
   return(list(pheno    = pheno_name, 
               var_type = column_name_no_pheno))
 }
+
+pheno_name_test <- map(colnames(plant_row_summary)[2:ncol(plant_row_summary)], get_pheno_name)
+
+test <- map_chr(pheno_name_test, function(x) pluck(x, "pheno")) %>% rle()
+
+c(" ", test$lengths %>% set_names(test$values))
+
+
+# A function that makes a kable table with a grouped header above each of the phenotypes
+make_kable_table <- function(phenotype_summary){
+  
+  pheno_col_indices <- map(colnames(phenotype_summary)[2:ncol(phenotype_summary)], get_pheno_name)
+  
+  # Make a vector of header spans
+  pheno_rle    <- test <- map_chr(pheno_col_indices, function(x) pluck(x, "pheno")) %>% rle()
+  header_spans <- c(" ", test$lengths %>% set_names(test$values))
+  
+  # Use the footnote_marker_alphabet to add footnote symbols to the header spanners
+  names(header_spans)[2] <- paste0(names(header_spans)[2], footnote_marker_alphabet(1))
+  
+  # The column names without the phenotypes appended
+  col_names_no_pheno <- map_chr(pheno_col_indices, function(x) pluck(x, "var_type"))
+  
+  new_colnames <- colnames(phenotype_summary)
+  new_colnames[2:ncol(phenotype_summary)] <- col_names_no_pheno
+
+  colnames(phenotype_summary) <- new_colnames
+  
+  kbl_table <- phenotype_summary %>% 
+    kable(booktabs = TRUE, caption = "Mian Plant Rows Cross Summary 2021", digits = 2, align = rep('c', ncol(phenotype_summary))) %>%
+    kable_styling(full_width = TRUE, position = "center") %>%
+    add_header_above(header = header_spans)
+  
+  return(kbl_table)
+}
+
